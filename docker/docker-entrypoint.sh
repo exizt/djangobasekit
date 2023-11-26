4392d2cfafa4ab60ea7b3a160ca2d145ee9aca8e
exizt <e2xist@gmail.com>
2023-11-27 02:15:43 +0900

    docker

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -18,5 +18,13 @@ cd /srv/app
 # 개발 모드에서만 필요함.
 # pip install django-debug-toolbar
 
-echo "run django server"
-python manage.py runserver 0.0.0.0:8000
+
+if [ -f /srv/app/.env ]; then
+    # .env 파일이 설정되어있을 때에만 실행. 아닌 경우에는 의미가 없는 듯.
+    echo "run django server"
+    python manage.py runserver 0.0.0.0:80
+else
+    echo "[entrypoint.sh] .env not found"
+    exit 1
+fi
+
